cf7eaf0915bdc44f4a46d26ecb03eb5a8e6d4b81
Matthew Hotchkiss <hotchkma@flip3.engr.oregonstate.edu>
2021-05-14 15:08:26 -0700

    basic functionality

--- WordCount.py
+++ WordCount.py
@@ -1,1 +1,11 @@
+def WordCount(x):
+	count = 1
+	for i in range(int(len(x))):
+			if(x[i]==" "):
+					count=count+1
+	return count
 
+print(WordCount("hello there"))
+print(WordCount("hi, my name is Matthew"))
+print(WordCount("haha that's so funny bro!"))
+print(WordCount("This is a long ass sentence and god damn there are some words in it!"))
--- palindrome.py
+++ palindrome.py
@@ -1,1 +1,6 @@
-
+def palindrome(x):
+	l = x
+	for i in range(int(len(l)/2)):
+		if (l[i]!=l[len(l)-1-i]):
+			return 0
+	return 1
