0bdb5afa0ddc6b94375d17e0bf52699aae7fb2e1
Matthew Hotchkiss <hotchkma@flip3.engr.oregonstate.edu>
2021-05-14 15:22:10 -0700

    added unittest and adjusted WordCount

--- WordCount.py
+++ WordCount.py
@@ -5,7 +5,3 @@ def WordCount(x):
 					count=count+1
 	return count
 
-print(WordCount("hello there"))
-print(WordCount("hi, my name is Matthew"))
-print(WordCount("haha that's so funny bro!"))
-print(WordCount("This is a long ass sentence and god damn there are some words in it!"))
